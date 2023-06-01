a261a10f85f8ef5903ac9bb18bab0a721d21e03a
Willian de Souza Fernandes <109435198+wfernandes79@users.noreply.github.com>
2023-06-01 17:19:33 -0300

    ex08

---
+++ JS/ex08.js
@@ -0,0 +1,6 @@
+var vel = 99
+console.log(`A velocidade do seu carro é ${vel} KM/h!`)
+if (vel > 100) { // É uma condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
