1f36679a655578f83c81c68e50a7d58ddbb61c0d
Willian de Souza Fernandes <109435198+wfernandes79@users.noreply.github.com>
2023-07-27 13:45:17 -0300

    2707

---
+++ modulo E/ambiente.js
@@ -0,0 +1,25 @@
+var c = 1
+while (c <= 5) {
+    console.log('Tudo bem?')
+    c++
+}
+
+
+
+
+/*
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
