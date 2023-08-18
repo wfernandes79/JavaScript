83350c15bc1ba37562ac6144deba5d8b9333f538
Willian de Souza Fernandes <109435198+wfernandes79@users.noreply.github.com>
2023-08-18 09:58:38 -0300

    aula17n

--- modulo F/aula17/objeto01.js
+++ modulo F/aula17/objeto01.js
@@ -1,1 +1,21 @@
+
+let amigo = {
+    nome: 'Willian',
+    sexo: 'M',
+    peso: 102,
+    engordar(p=0){
+      
+        this.peso += p
+    }
+}
+amigo.engordar(-1)
+
+console.log(`${amigo.nome}, pesa ${amigo.peso} kg.`)
+
+
+if(amigo.peso > 102){
+    console.log('Engordou')
+
+   } else { 
+    console.log('Emagreceu')
 }
